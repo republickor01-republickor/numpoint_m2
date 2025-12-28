0c184b2e72a4f90ece77afdf56dddf7a779fc203
republickor01-republickor <republickor01@gmail.com>
2025-12-28 22:01:10 +0900

    Add files via upload

--- soundManager.js
+++ soundManager.js
@@ -9,18 +9,29 @@ const SoundManager = {
   },
 
   playWrong() {
-    this.wrong.currentTime = 0;
-    this.wrong.play();
+    speechSynthesis.cancel(); // 이전 말 중단
+    speak("다시생각해봐");
+    //this.wrong.currentTime = 0;
+    //this.wrong.play();
   },
 
   speak(text) {
     const u = new SpeechSynthesisUtterance(text);
     u.lang = "ko-KR";
-    u.rate = 1.0;
+    u.rate = 1.3;
     u.pitch = 1.2;
     speechSynthesis.cancel(); // 겹침 방지
     speechSynthesis.speak(u);
   }
+  
 };
 
+
 export default SoundManager;
+function speak(text) {
+  const utter = new SpeechSynthesisUtterance(text);
+  utter.lang = "ko-KR";   // 한국어
+  utter.rate = 1.0;       // 말하는 속도
+  utter.pitch = 1.0;      // 음 높이
+  speechSynthesis.speak(utter);
+}
